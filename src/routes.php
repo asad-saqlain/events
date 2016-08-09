168629cd5b190861aad11e1455b78aae998af14d
Mubin <mbnmughal30@gmail.com>
2016-08-09 18:00:18 +0500

    added new routes

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,5 @@
 
 Route::get('events/global', 'Mubin\Events\EventsController@globalEvent');
 Route::get('events/user', 'Mubin\Events\EventsController@userEvent');
+Route::get('events/backend_user', 'Mubin\Events\EventsController@userEvent');
+Route::get('events/backend_global', 'Mubin\Events\EventsController@updateCode');
