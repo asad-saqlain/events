cb3a28183a931f085e4f0b72377cdbc61f2181b6
Mubin <mbnmughal30@gmail.com>
2016-08-09 11:58:48 +0500

    added Feed Consumer Library

---
+++ src/BgEvents/FeedConsumer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mubin
+ * Date: 8/9/2016
+ * Time: 11:33 AM
+ */
+
+namespace Mubin\Events\BgEvents;
+
+use r;
+
+class FeedConsumer
+{
+    /**
+     * @param $table String  Table name to watch for feeds.
+     */
+    public function watch($table){
+        $connection = $this->openConnection();
+
+        $feed = r\table($table)->changes()->run($connection);
+
+        foreach($feed as $change){
+            print_r($feed);
+        }
+    }
+
+    /**
+     * @return r\Connection
+     */
+    public function openConnection(){
+        $host = config('bgevents.rethink_db.host');
+        $port = config('bgevents.rethink_db.port');
+        $db = config('bgevents.rethink_db.db');
+        $connection = r\connect($host, $port, $db);
+
+        return $connection;
+    }
+}
