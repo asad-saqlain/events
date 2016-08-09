b2cab6413bb196292f2f4d92d98f52729865c82a
Mubin <mbnmughal30@gmail.com>
2016-08-09 17:59:15 +0500

    updated Feed Consumer
    updated Feed Consumer to use Subscriber library

--- src/BgEvents/FeedConsumer.php
+++ src/BgEvents/FeedConsumer.php
@@ -9,9 +9,16 @@
 namespace Mubin\Events\BgEvents;
 
 use r;
-
+use Mubin\Events\BgEvents\Subscriber;
 class FeedConsumer
 {
+    protected $subscriber;
+
+    public function __construct(Subscriber $subscriber)
+    {
+        $this->subscriber = $subscriber;
+    }
+
     /**
      * @param $table String  Table name to watch for feeds.
      */
@@ -21,7 +28,11 @@ class FeedConsumer
         $feed = r\table($table)->changes()->run($connection);
 
         foreach($feed as $change){
-            print_r($change);
+            if($change['new_val']['component'] == 'general'){
+                $event_name = $change['new_val']['message_payload']['event_name'];
+                $event_meta = $change['new_val']['message_payload']['event_meta'];
+                $this->subscriber->fire(['event_name' => $event_name, 'event_meta' =>$event_meta]);
+            }
         }
     }
 
