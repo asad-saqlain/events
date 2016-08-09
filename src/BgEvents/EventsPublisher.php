845792b93f08311e80b6470dcd7f1fcfdc419af5
Mubin <mbnmughal30@gmail.com>
2016-08-09 17:58:45 +0500

    Event Publisher udpated

--- src/BgEvents/EventsPublisher.php
+++ src/BgEvents/EventsPublisher.php
@@ -33,11 +33,12 @@ class EventsPublisher
      * Publish message to rethink db via node api.
      * @param $type string [global, user, backend]
      * @param $component    string  ["general", "criteria", "groups", "season", "profiles", "settings", "pricing_mode", "price_update"]
-     * @param string $event_name    string  event name to fire[only in case of backend]
-     * @param array $data   data that need to pass to rethink db.
-     * @return void
+     * @param string $event_name string  event name to fire[only in case of backend]
+     * @param array $data data that need to pass to rethink db.
+     * @param bool $per_user
+     * @return bool|String
      */
-    public function publish($type, $component, $event_name = '', $data = []){
+    public function publish($type, $component, $event_name = '', $data = [], $per_user = false){
         $emit = [
             "component_name" => $component,
             "message_payload" => [
@@ -46,6 +47,7 @@ class EventsPublisher
         ];
         if($type === "global"){
             $this->globalEvent($emit);
+            return "Global Event Published";
         }
         elseif($type === "user"){
             $emit['api_key'] = $data['api_key'];
@@ -54,8 +56,9 @@ class EventsPublisher
             $emit['user_email'] = $data['user_email'];
             $emit['account_type'] = $data['account_type'];
             $this->localEvent($emit);
+            return 'User Event Published';
         }
-        elseif ($type == 'backend')
+        elseif ($type == 'backend' && $per_user)
         {
             if(isset($data['api_key'])){
                 $emit['api_key'] = $data['api_key'];
@@ -63,10 +66,21 @@ class EventsPublisher
                 $emit['user_id'] = $data['user_id'];
                 $emit['account_type'] = $data['account_type'];
                 $emit['user_email'] = $data['user_email'];
-                $emit['event_name'] = $event_name;
-                $this->backendNotification($emit);
+                $emit['message_payload']['event_name'] = $event_name;
+                $emit['message_payload']['event_meta'] = $data['event_meta'];
+                $this->backendNotificationPerUser($emit);
             }
+            return 'Backend Event Published';
         }
+        elseif ($type == 'backend' && !$per_user)
+        {
+            $emit['message_payload']['event_name'] = $event_name;
+            $emit['message_payload']['event_meta'] = $data['event_meta'];
+            $this->backendGlobalNotification($emit);
+
+            return 'Backend Event Published';
+        }
+        return false;
     }
 
     /**
@@ -97,8 +111,18 @@ class EventsPublisher
      * @param array $emit
      * @return void
      */
-    private function backendNotification($emit){
-        $this->guzzle->post(config('bgevents.push_notification'), [
+    private function backendNotificationPerUser($emit){
+        $this->guzzle->post(config('bgevents.push_user'), [
+            'headers' => ['content-type' => 'application/json'],
+            'body' => json_encode($emit)
+        ]);
+    }
+    /**
+     * @param array $emit
+     * @return void
+     */
+    private function backendGlobalNotification($emit){
+        $this->guzzle->post(config('bgevents.push_global'), [
             'headers' => ['content-type' => 'application/json'],
             'body' => json_encode($emit)
         ]);
