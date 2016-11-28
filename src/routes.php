26307f876fb22a6c8c95ac0ae95a290a00578f15
Mubin <mubin@brokergenius.com>
2016-11-28 18:16:18 +0500

    routes reformat according PSR-2

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Mubin
- * Date: 8/8/2016
- * Time: 10:19 AM
- */
 
 Route::get('events/global', 'Mubin\Events\EventsController@globalEvent');
 Route::get('events/user', 'Mubin\Events\EventsController@userEvent');
